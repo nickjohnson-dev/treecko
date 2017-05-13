b24de4621fcddeb0cdf1c6091d60b3adccf024a3
Nick Johnson <nickjohnson.dev@gmail.com>
2017-05-13 18:20:07 -0500

    Added treecko image to README and fixed root export

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # treecko
 Functional tree helpers
+
+![What is a treecko][treecko]
+
+[treecko]: http://vignette3.wikia.nocookie.net/pokemontowerdefense3/images/c/c1/Treecko.jpg/revision/latest?cb=20140803080131
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-export each from './each';
+import each from './each';
+import findOr from './findOr';
+import flatten from './flatten';
+import flattenToMap from './flattenToMap';
+import map from './map';
+import mapWhen from './mapWhen';
+import reduce from './reduce';
+import reduceAncestryBy from './reduceAncestryBy';
+import reject from './reject';
+import replaceBy from './replaceBy';
+import superflatten from './superflatten';
+
+export default {
+  each,
+  findOr,
+  flatten,
+  flattenToMap,
+  map,
+  mapWhen,
+  reduce,
+  reduceAncestryBy,
+  reject,
+  replaceBy,
+  superflatten,
+};
