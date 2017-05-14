e0f24274f3ec1c5b2f21225e95252271b68e80d1
Nick Johnson <nickjohnson.dev@gmail.com>
2017-05-13 19:41:57 -0500

    Fixed export

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
+import addChildBy from './addChildBy';
+import changeParent from './changeParent';
 import each from './each';
 import findOr from './findOr';
 import flatten from './flatten';
 import flattenToMap from './flattenToMap';
-import map from './map';
-import mapWhen from './mapWhen';
+import hardMap from './hardMap';
+import hardMapBy from './hardMapBy';
 import reduce from './reduce';
 import reduceAncestryBy from './reduceAncestryBy';
 import reject from './reject';
 import replaceBy from './replaceBy';
+import replaceChildrenBy from './replaceChildrenBy';
+import softMap from './softMap';
+import softMapBy from './softMapBy';
 import superflatten from './superflatten';
 
 export default {
+  addChildBy,
+  changeParent,
   each,
   findOr,
   flatten,
   flattenToMap,
-  map,
-  mapWhen,
+  hardMap,
+  hardMapBy,
   reduce,
   reduceAncestryBy,
   reject,
   replaceBy,
+  replaceChildrenBy,
+  softMap,
+  softMapBy,
   superflatten,
 };
